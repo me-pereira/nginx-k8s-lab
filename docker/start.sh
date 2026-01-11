556b47db10bd50bb76140f80c587d91bf931d38b
me-pereira <marcel_eberlin@hotmail.com>
2026-01-11 02:19:12 -0300

    DateTime adjusted on start.sh and index.html; Portainer installed

--- docker/start.sh
+++ docker/start.sh
@@ -1,21 +1,19 @@
 #!/bin/sh
 
-# Substitui hostname, IP, versão do nginx e versão do SO no index.html
 NGINX_VERSION=$(nginx -v 2>&1 | cut -d"/" -f2)
 OS_VERSION=$(grep PRETTY_NAME /etc/os-release | cut -d'=' -f2 | tr -d '"')
+DATETIME=$(date '+%d/%m/%Y %H:%M:%S')
 
-sed -i "s/__HOSTNAME__/$(hostname)/g" /usr/share/nginx/html/index.html
-sed -i "s/__IP__/$(hostname -i)/g" /usr/share/nginx/html/index.html
-sed -i "s/__NGINX_VERSION__/$NGINX_VERSION/g" /usr/share/nginx/html/index.html
-sed -i "s#__OS_VERSION__#$OS_VERSION#g" /usr/share/nginx/html/index.html
+sed -i "s|__HOSTNAME__|$(hostname)|g" /usr/share/nginx/html/index.html
+sed -i "s|__IP__|$(hostname -i)|g" /usr/share/nginx/html/index.html
+sed -i "s|__NGINX_VERSION__|$NGINX_VERSION|g" /usr/share/nginx/html/index.html
+sed -i "s|__OS_VERSION__|$OS_VERSION|g" /usr/share/nginx/html/index.html
+sed -i "s|__DATETIME__|$DATETIME|g" /usr/share/nginx/html/index.html
 
-# Inicia o Nginx em background
 nginx -g "daemon off;" &
 
-# Loop para load dinâmico via ConfigMap
 while true; do
   if [ -f /etc/load_flag/flag ] && [ "$(cat /etc/load_flag/flag)" = "true" ]; then
-    # Gera carga de CPU quando ENABLED
     stress --cpu 1 --timeout 5
   else
     sleep 1
